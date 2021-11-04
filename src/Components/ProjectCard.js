3e3e627a4e79433c1b3a384751f6312d96892172
Sizwe <sizwemasemola1@gmail.com>
2021-11-04 14:03:14 +0200

    added image overlay on hover

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./ProjectCard.css";
 
-function ProjectCard({project}) {
+function ProjectCard({ project }) {
   return (
-      <div className="card-container">
-        <h3 className="project-name">{project.project_name}</h3>
-        <p className="project-type">{project.type}</p>
-        <img src={project.project_image} alt={project.project_name} />
-      </div>
+    <div className="card-container">
+      <a href={project.site}>
+        <div className="card">
+          <div className="card-text">
+            <h3 className="project-name">{project.project_name}</h3>
+            <p className="project-type">{project.type}</p>
+          </div>
+          <div className="card-img">
+            <img src={project.project_image} alt={project.project_name} />
+          </div>
+        </div>
+      </a>
+    </div>
   );
 }
 
